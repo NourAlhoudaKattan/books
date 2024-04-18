a99163c79cd91ec98b6c9d5762c5c153521edd59
NourAlhoudaKattan <nwralhdyqtan14@gmail.com>
2024-04-18 14:05:16 +0200

    fix: some bug

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from '../Contexts/theme.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-  <BrowserRouter>
+  <BrowserRouter basename='/books/'>
    <React.StrictMode>
     <ThemeProvider>
     <App />
